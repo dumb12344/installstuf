daa7ac0dd2b4af0cff252e5d55423240e6e98f51
dumb12344 <167947276+dumb12344@users.noreply.github.com>
2025-02-24 23:38:50 +0000

    more fix

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 read -n1 -r -p "Connect to internet"
-if [ ~ -d .config ] then
+if [ ! -d .config/ ]; then
     git clone https://github.com/dumb12344/installstuf.git
     cd installstuf
 fi
-curl https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
+curl https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o chrome.deb
+xdg-open https://romsfun.com/download/super-smash-bros-melee-27505/4
 read -n1 -r -p "Download ssbm and put ssbm.rvz into this folder"
 sudo apt install wget dolphin-emu neovim plasma-workspace-wallpapers
 sudo dpkg -i chrome.deb
