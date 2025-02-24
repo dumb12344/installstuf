b492453d4e3bd29dcd807100cc346d9594dee9e2
dumb12344 <167947276+dumb12344@users.noreply.github.com>
2025-02-24 23:19:18 +0000

    fix
    	modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-read -n1
-if [ ~ -f chrome.deb ] then
+read -n1 -r -p "Connect to internet"
+if [ ~ -d .config ] then
     git clone https://github.com/dumb12344/installstuf.git
     cd installstuf
 fi
+curl https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
+read -n1 -r -p "Download ssbm and put ssbm.rvz into this folder"
 sudo apt install wget dolphin-emu neovim plasma-workspace-wallpapers
 sudo dpkg -i chrome.deb
 mkdir ~/.roms/
